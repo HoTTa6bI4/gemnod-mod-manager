cf0d16e9d2f54f769baaae8524e0eb9bea95047f
Александр Штенцов <amshtents@yandex.ru>
2025-06-04 15:46:16 +0300

    Absorbed TypesXmlHandler class by HeroesVFileInspector class, and delegated all its functionality forward.

--- src/heroes_v_file_seeker.py
+++ src/heroes_v_file_seeker.py
@@ -2,7 +2,7 @@ import os
 import shutil
 from file_seeker import *
 from hashlib import md5
-
+from xml.etree import ElementTree as ET
 
 # Default storage of indexed files
 # todo: make it users environment variable
@@ -24,13 +24,11 @@ def filehash(file_path):
 
 
 class HeroesVFileInspector:
-
     _instance = None
 
     def __new__(cls, *args, **kwargs):
         if cls._instance is None:
             cls._instance = super(HeroesVFileInspector, cls).__new__(cls)
-            print("New instance created.")
         return cls._instance
 
     def instance(self=None):
@@ -49,95 +47,62 @@ class HeroesVFileInspector:
 
     indexes_dir = '../index/indexes/'
 
-    # Simple check whether chosen folder is Heroes V game folder
-    #
-    def __isheroesv(self):
-        if os.path.exists(os.path.join(self.game_root, 'bin/')):
-            if os.path.exists(os.path.join(self.game_root, 'data/')):
-                return True
-        return False
+    tables = {
+        "artifacts": {'ids': {}, 'server_ptr': '7b059128'},
+        "creatures": {'ids': {}, 'server_ptr': 'dd04dd1e'},
+        "spells": {'ids': {}, 'server_ptr': '8d02a80a'},
+        "skills": {'ids': {}, 'server_ptr': '8c029e0a'},
+        "players_filter": {'ids': {}, 'server_ptr': '10000001'},
+        "talkbox_close_modes": {'ids': {}, 'server_ptr': '10000006'},
+    }
 
     def __init__(self, game_root, indexed_places_file=default_indexed_places):
-        print("Instance inited.")
         self.game_root = game_root
-        if not self.__isheroesv():
+        if not self.__isHeroesV():
             raise NotADirectoryError("Current directory is not a Heroes V game folder!")
         self.unindexed_places = []
         self.indexes_dictionary = {}
         self.indexed_places_file = indexed_places_file
-        # Read existing indexes dictionary from file
-        if os.path.exists(indexed_places_file):
-            with open(indexed_places_file) as ind:
-                try:
-                    for line in ind:
-                        hashsum, index = line.replace('\r', '').replace('\n', '').split(' ')
-                        self.indexes_dictionary[hashsum] = index
-                except Exception as error:
-                    print("<ERROR> Uknown error while parsing indexes database")
-                    print("<ERROR", error, ">")
-        else:
-            # Create empty one for write
-            dirs, filename = os.path.split(indexed_places_file)
-            os.makedirs(dirs, exist_ok=True)
-            with open(indexed_places_file, 'w'):
-                pass
+        self.__readIndexes()
+        # Find not indexed (but possible to) places
+        for folder, extension in self.inspected_archives:
+            inspected_folder = os.path.join(self.game_root, folder)
+            if os.path.exists(inspected_folder):
+                for any_file in os.listdir(inspected_folder):
+                    file_abs_path = os.path.join(inspected_folder, any_file)
+                    if any_file.endswith(extension):
+                        file_hash = filehash(file_abs_path)
+                        index = self.__getIndex(file_hash)
+                        if index is None:
+                            print(f"> Not indexed place: {any_file} | {file_hash}")
+                            self.unindexed_places.append(file_abs_path)
 
-    def __getindex(self, hashsum):
+    def __getIndex(self, hashsum):
         if hashsum in self.indexes_dictionary.keys():
             return self.indexes_dictionary[hashsum]
         else:
             return None
 
-    def get(self, rel_path):
-
-        # print(f"Searching for {rel_path} in {self.game_root}")
-        rel_path = rel_path.replace('\\', '/')
-        if rel_path.startswith('/'):
-            rel_path = rel_path.replace('/', '', 1)
-
-        # Get last version of 'rel_path' for installed game
-        #
-        seeker = None
-        final_seeker = None
-        contents = []
-        last_modification_time = 0.0
-
-        for inspected_folder in self.inspected_folders:
-            seeker = FolderSeeker(os.path.join(self.game_root, inspected_folder))
-            time = seeker.getmtime(rel_path)
-            if time > last_modification_time:
-                final_seeker = seeker
-                last_modification_time = time
-
-        for folder, extension in self.inspected_archives:
-            inspected_folder = os.path.join(self.game_root, folder)
-            for any_file in os.listdir(inspected_folder):
-                file_abs_path = os.path.join(inspected_folder, any_file)
-                if any_file.endswith(extension):
-                    file_hash = filehash(file_abs_path)
-                    index = self.__getindex(file_hash)
-                    # If no index for this archive, use default seeker
-                    if index is None:
-                        print(f"> Unindexed place: {any_file} | {file_hash}")
-                        seeker = ArchivesSeeker(inspected_folder, any_file)
-                        self.unindexed_places.append(file_abs_path)
-                    else:
-                        # print(f"> Indexed place: {any_file} | {file_hash}")
-                        seeker = IndexedArchiveSeeker(inspected_folder, any_file, index)
-                        pass
-                    mtime = seeker.getmtime(rel_path)
-                    if mtime > last_modification_time:
-                        final_seeker = seeker
-
-        if final_seeker is not None:
-            contents = final_seeker.getfile(rel_path)
+    def __readIndexes(self):
+        # Read existing indexes dictionary from file
+        if os.path.exists(self.indexed_places_file):
+            with open(self.indexed_places_file) as ind:
+                try:
+                    for line in ind:
+                        hashsum, index = line.replace('\r', '').replace('\n', '').split(' ')
+                        self.indexes_dictionary[hashsum] = index
+                except Exception as error:
+                    print("<ERROR> Unknown error while parsing indexes database")
+                    print("<ERROR", error, ">")
         else:
-            raise FileNotFoundError(f"Inspector didn't find any file '{rel_path}'")
+            # Create empty one for write
+            dirs, filename = os.path.split(self.indexed_places_file)
+            os.makedirs(dirs, exist_ok=True)
+            with open(self.indexed_places_file, 'w'):
+                pass
 
-        return contents
-
-    def indexNewPlaces(self):
-        # print(f"> Indexing: {self.unindexed_places}")
+    def __indexNewPlaces(self):
+        print(f"> Indexing: {self.unindexed_places}")
         for place in self.unindexed_places:
             # Index dir name is idexed file hash
             hashsum = filehash(place)
@@ -164,16 +129,16 @@ class HeroesVFileInspector:
             print(f"> Created index for {place}...")
             writer.commit()
 
-        self.writeIndexes()
+        self.__writeIndexes()
 
-    def writeIndexes(self):
+    def __writeIndexes(self):
         with open(self.indexed_places_file, 'w') as indexes_file:
             for key, value in self.indexes_dictionary.items():
                 string = str(key) + ' ' + str(value) + '\r'
                 indexes_file.write(string)
         # print(self.indexes_dictionary)
 
-    def flush(self):
+    def __flush(self):
         # Find unknown indexes
         # Remove unknown indexes
         for dir in os.listdir(self.indexes_dir):
@@ -185,19 +150,104 @@ class HeroesVFileInspector:
                 print(f"> Flushing unused index place: {dir}...")
                 shutil.rmtree(os.path.join(self.indexes_dir, dir))
 
+    # Simple check whether chosen folder is Heroes V game folder
+    #
+    def __isHeroesV(self):
+        if os.path.exists(os.path.join(self.game_root, 'bin/')):
+            if os.path.exists(os.path.join(self.game_root, 'data/')):
+                return True
+        return False
+
+    def get(self, rel_path):
+
+        # print(f"Searching for {rel_path} in {self.game_root}")
+        rel_path = rel_path.replace('\\', '/')
+        if rel_path.startswith('/'):
+            rel_path = rel_path.replace('/', '', 1)
+
+        # Get last version of 'rel_path' for installed game
+        #
+        seeker = None
+        final_seeker = None
+        contents = []
+        last_modification_time = 0.0
+
+        for folder in self.inspected_folders:
+            inspected_fodler = os.path.join(self.game_root, folder)
+            if os.path.exists(inspected_fodler):
+                seeker = FolderSeeker(inspected_fodler)
+                time = seeker.getmtime(rel_path)
+                if time > last_modification_time:
+                    final_seeker = seeker
+                    last_modification_time = time
+
+        for folder, extension in self.inspected_archives:
+            inspected_folder = os.path.join(self.game_root, folder)
+            if os.path.exists(inspected_folder):
+                for any_file in os.listdir(inspected_folder):
+                    file_abs_path = os.path.join(inspected_folder, any_file)
+                    if any_file.endswith(extension):
+                        file_hash = filehash(file_abs_path)
+                        index = self.__getIndex(file_hash)
+                        # If no index for this archive, use default seeker
+                        if index is None:
+                            # print(f"> Unindexed place: {any_file} | {file_hash}")
+                            seeker = ArchivesSeeker(inspected_folder, any_file)
+                            self.unindexed_places.append(file_abs_path)
+                        else:
+                            # print(f"> Indexed place: {any_file} | {file_hash}")
+                            seeker = IndexedArchiveSeeker(inspected_folder, any_file, index)
+                            pass
+                        mtime = seeker.getmtime(rel_path)
+                        if mtime > last_modification_time:
+                            final_seeker = seeker
+
+        if final_seeker is not None:
+            contents = final_seeker.getfile(rel_path)
+        else:
+            raise FileNotFoundError(f"Inspector didn't find any file '{rel_path}'")
+
+        return contents
+
+    def getNumericID(self, table, string_id):
+        if table not in self.tables.keys():
+            raise AttributeError(1, f"Invalid table name {table}")
+        if string_id not in self.tables[table]['ids'].keys():
+            raise AttributeError(1, f"Invalid ID [{string_id}] for table [{table}]")
+        numeric_id = self.tables[table]['ids'][string_id]
+        return numeric_id
+
+    def updateTypes(self):
+
+        contents = self.get("types.xml")
+        document_root = ET.fromstringlist(contents)
+
+        for item in document_root.findall("SharedClasses/Item"):
+            server_ptr = item.find("__ServerPtr").text
+
+            for table_name, table_props in self.tables.items():
+                if server_ptr == table_props['server_ptr']:
+                    for entry in item.findall("Entries/Item"):
+                        STRING_ID = entry.find("Name").text
+                        numeric_id = entry.find("Value").text
+                        self.tables[table_name]['ids'][STRING_ID] = numeric_id
+
+    def updateIndexes(self):
+        self.__readIndexes()
+        self.__indexNewPlaces()
+        self.__writeIndexes()
+        self.__flush()
+
 
 if __name__ == "__main__":
-    # my_inspc = HeroesVFileInspector("S:\\Games\\Nival Interactive\\Heroes V Clear Version\\")
-    # my_inspc2 = HeroesVFileInspector("S:\\Games\\Nival Interactive\\Heroes of Might and Magic V\\")
-    my_inspc3 = HeroesVFileInspector.instance()
-    # print(my_inspc is my_inspc2)
-    print(my_inspc3 is not None)
+    my_inspc = HeroesVFileInspector("S:\\Games\\Nival Interactive\\Heroes of Might and Magic V - Tribes of the East\\")
+
+    my_inspc.updateIndexes()
+    # my_inspc.updateTypes()
+
     now = time.time()
-    repeats = 1
+    repeats = 10
     file = []
-    # for i in range(repeats):
-    #     file = my_inspc.get("Folder/Text.txt")
-    # print(f"{repeats} searches time: {time.time() - now} with {(time.time() - now)/repeats} average search time")
-    # my_inspc.indexNewPlaces()
-    # my_inspc.writeIndexes()
-    # my_inspc.flush()
+    for i in range(repeats):
+        file = my_inspc.get("types.xml")
+    print(f"{repeats} searches time: {time.time() - now} with {(time.time() - now)/repeats} average search time")
